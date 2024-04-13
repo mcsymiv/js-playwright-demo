e5501986bdd777526e26d288b8877bf61e450802
mcsymiv <s.mcsymiv@gmail.com>
2024-04-13 20:28:24 +0300

    simple-git commit message, add screenshots

--- settings/testSettings.ts
+++ settings/testSettings.ts
@@ -1,3 +1,4 @@
 import appRoot from 'app-root-path';
 
 export const screenshotsPath = appRoot.path + '/screenshots/screenshot.png'
+export const appRepoName = appRoot.path.split('/').slice(-1)[0];
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,29 @@
 import { test, expect } from '@playwright/test';
-import { screenshotsPath } from '../settings/testSettings';
-import { simpleGit, SimpleGit, CleanOptions, StatusResult  } from 'simple-git';
+import { githubAccessToken  } from '../config/envs';
+import { screenshotsPath, appRepoName } from '../settings/testSettings';
+import { simpleGit, SimpleGit, StatusResult  } from 'simple-git';
 
 test.describe('simple-git test', async () => {
 
   test.afterAll(async () => {
-    const git: SimpleGit = simpleGit().clean(CleanOptions.FORCE);
+    const git: SimpleGit = simpleGit();
     const status: StatusResult = await git.status()
+    const gitHubUrl = `https://${githubAccessToken}@github.com/mcsymiv/${appRepoName}.git`;
 
-    console.log(status.modified);
+    try {
+      // await git.addConfig('user.email','s.mcsymiv@gmail.com');
+      // await git.addConfig('user.name','mcsymiv');
+
+      // await git.addRemote('origin', gitHubUrl);
+
+      await git.add(status.modified);
+      await git.commit('simple-git commit message, add screenshots');
+      await git.push('origin', 'main');
+
+    } catch (error) {
+      console.log(error);
+    }
+
   });
 
   test('get started link', async ({ page }) => {
@@ -17,7 +32,7 @@ test.describe('simple-git test', async () => {
     // Click the get started link.
     await page.getByRole('link', { name: 'Get started' }).click();
 
-    await page.screenshot({ path: screenshotsPath, fullPage: false });
+    await page.screenshot({ path: screenshotsPath, fullPage: true });
 
     // Expects page to have a heading with the name of Installation.
     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
